a006bda749ee7bc17c6493c8be9de75db8dba7e1
harelbaruchi <harelbaruchi@gmail.com>
2024-03-05 22:44:09 +0200

    pushing to new repo

---
+++ EmployeeApp/urls.py
@@ -0,0 +1,13 @@
+from django.urls import re_path
+from EmployeeApp import views
+
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    re_path(r'^department/$',views.departmentApi),
+    re_path(r'^department/([0-9]+)$',views.departmentApi),
+     re_path(r'^employee/$',views.employeesApi),
+    re_path(r'^employee/([0-9]+)$',views.employeesApi),
+    re_path(r'^SaveFile$', views.SaveFile)
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
